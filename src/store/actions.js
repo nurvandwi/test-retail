ae946e3d952613c2aea52cdedb187e1fae9db439
Robby Benta <robbybenta7@gmail.com>
2020-09-09 14:48:46 +0700

    update https

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export function getProducts({ commit }) {
   console.log(this);
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/products")
+    .get("https://shayna-backend.belajarkoding.com/api/products")
     .then((response) => {
       commit("SET_PRODUCTS", response.data.data.data);
     });
@@ -10,7 +10,7 @@ export function getProducts({ commit }) {
 export function getEwallets({ commit }) {
   console.log(this);
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/products")
+    .get("https://shayna-backend.belajarkoding.com/api/products")
     .then((response) => {
       commit("SET_EWALLETS", response.data.data.data);
     });
@@ -18,7 +18,7 @@ export function getEwallets({ commit }) {
 export function getPoinCashs({ commit }) {
   console.log(this);
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/products")
+    .get("https://shayna-backend.belajarkoding.com/api/products")
     .then((response) => {
       commit("SET_POINCASHS", response.data.data.data);
     });
@@ -26,7 +26,7 @@ export function getPoinCashs({ commit }) {
 export function getRebates({ commit }) {
   console.log(this);
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/products")
+    .get("https://shayna-backend.belajarkoding.com/api/products")
     .then((response) => {
       commit("SET_REBATES", response.data.data.data);
     });
@@ -34,7 +34,7 @@ export function getRebates({ commit }) {
 export function getProduct({ commit }, { id }) {
   console.log(this);
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/products", {
+    .get("https://shayna-backend.belajarkoding.com/api/products", {
       params: {
         id,
       },
@@ -49,7 +49,7 @@ export function addProductToCart({ commit }, { product, quantity }) {
     product,
     quantity,
   });
-  axios.post("http://shayna-backend.belajarkoding.com/api/cart", {
+  axios.post("https://shayna-backend.belajarkoding.com/api/cart", {
     product_id: product.id,
     quantity,
   });
@@ -59,7 +59,7 @@ export function addEwalletToCart({ commit }, { ewallet, quantity }) {
     ewallet,
     quantity,
   });
-  axios.post("http://shayna-backend.belajarkoding.com/api/cart", {
+  axios.post("https://shayna-backend.belajarkoding.com/api/cart", {
     ewallet_id: ewallet.id,
     quantity,
   });
@@ -70,7 +70,7 @@ export function addPoinCashToCart({ commit }, { poincash, quantity }) {
     poincash,
     quantity,
   });
-  axios.post("http://shayna-backend.belajarkoding.com/api/cart", {
+  axios.post("https://shayna-backend.belajarkoding.com/api/cart", {
     ewallet_id: poincash.id,
     quantity,
   });
@@ -80,35 +80,35 @@ export function addRebateToCart({ commit }, { rebate, quantity }) {
     rebate,
     quantity,
   });
-  axios.post("http://shayna-backend.belajarkoding.com/api/cart", {
+  axios.post("https://shayna-backend.belajarkoding.com/api/cart", {
     rebate_id: rebate.id,
     quantity,
   });
 }
 export function getCartItems({ commit }) {
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/cart")
+    .get("https://shayna-backend.belajarkoding.com/api/cart")
     .then((response) => {
       commit("SET_CART", response.data);
     });
 }
 export function getEwalletItems({ commit }) {
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/cart")
+    .get("https://shayna-backend.belajarkoding.com/api/cart")
     .then((response) => {
       commit("SET_EWALLET", response.data);
     });
 }
 export function getPoincashItems({ commit }) {
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/cart")
+    .get("https://shayna-backend.belajarkoding.com/api/cart")
     .then((response) => {
       commit("SET_POINCASH", response.data);
     });
 }
 export function getRebateItems({ commit }) {
   axios
-    .get("http://shayna-backend.belajarkoding.com/api/cart")
+    .get("https://shayna-backend.belajarkoding.com/api/cart")
     .then((response) => {
       commit("SET_REBATE", response.data);
     });
@@ -116,11 +116,11 @@ export function getRebateItems({ commit }) {
 export function removeProductFromCart({ commit }, product) {
   commit("REMOVE_PRODUCT_FROM_CART", product);
   axios.delete(
-    `http://shayna-backend.belajarkoding.com/api/cart/${product.id}`,
+    `https://shayna-backend.belajarkoding.com/api/cart/${product.id}`,
     {}
   );
 }
 export function clearCartItems({ commit }) {
   commit("CLEAR_CART_ITEMS");
-  axios.delete("http://shayna-backend.belajarkoding.com/api/cart");
+  axios.delete("https://shayna-backend.belajarkoding.com/api/cart");
 }
