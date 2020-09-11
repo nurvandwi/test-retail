6667dfe31612546ab1f876e36519a11064edc2d1
Robby Benta <robbybenta7@gmail.com>
2020-09-11 16:35:10 +0700

    update font list transaksi

--- src/components/Main-Component/ListTransaksi.vue
+++ src/components/Main-Component/ListTransaksi.vue
@@ -1,10 +1,13 @@
 <template>
   <div class=" mt-4 pt-1 ">
-    <div class="col-12 align-self-center font14 bg-blue d-flex justify-content-between font-weight-bold">
-      <h1 class="font16 m-0 align-self-center font-weight-bold py-0 px-2">List Transaksi</h1>
+    <div
+      class="col-12 align-self-center font14 bg-blue d-flex justify-content-between font-weight-bold"
+    >
+      <h3 class="font16 m-0 align-self-center font-weight-bold py-0 px-2">
+        List Transaksi
+      </h3>
 
-        <p class="rounded-pill  m-0 font12 py-2 px-2 bg-purple">Lihat Semua</p>
-  
+      <p class="rounded-pill  m-0 font12 py-2 px-2 bg-purple">Lihat Semua</p>
     </div>
     <listItem :contentFor="'ListTransaksi'" />
   </div>
@@ -18,32 +21,9 @@ export default {
 };
 </script>
 <style scoped>
-.imagecustom {
-  background-size: cover;
-  height: 46px;
-}
-
-.shadow1 {
-  filter: drop-shadow(0px 10px 10px rgba(240, 248, 255, 0.1));
-}
-
-.br {
-  border: 1px #d3d3d3 solid;
-  border-radius: 15px;
-}
-
-.iconcustom {
-  background-size: cover;
-  height: 15px;
-}
-
 .bg-purple {
   background-color: #e3eeff;
   color: #4e37b2;
-}
-
-.font16 {
-  font-size: 16px;
 }
 
 .font14 {
@@ -51,5 +31,10 @@ export default {
 }
 .font12 {
   font-size: 12px;
+}
+@media screen and (max-width: 1000px) {
+  .font16 {
+    font-size: 16px;
+  }
 }
 </style>
