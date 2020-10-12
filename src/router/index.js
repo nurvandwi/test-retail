9ee30f057e3b89277a33732a83a558a144385cb9
nurvandwi <nurvandwi20@gmail.com>
2020-10-12 10:38:48 +0700

    update alltransaction-2

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,44 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from '../pages/Home.vue'
-import Catalog from '../pages/Catalog.vue'
-import Allproduct from '../pages/Allproduct.vue'
-import Product from '../pages/Product.vue'
-import Categories from '../pages/Categories.vue'
-import Ewallet from '../pages/Ewallet.vue'
-import PoinCash from '../pages/PoinCash.vue'
-import Rebate from '../pages/Rebate.vue'
-import Pulsa from '../pages/Pulsa.vue'
-import Quarter from '../pages/Quarter.vue'
-import Login from '../pages/Login.vue'
-import ProductDetails from '../pages/ProductDetails.vue'
-import PointCashDetails from '../pages/PointCashDetails.vue'
-import RebateDetails from '../pages/RebateDetails.vue'
-import PulsaDetails from '../pages/PulsaDetails.vue'
-import EwalletDetails from '../pages/EwalletDetails.vue'
-import TransactionDetails from '../pages/TransactionDetails.vue'
-import SuccessPage from '../pages/SuccessPage.vue'
-import Registrasi from '../pages/Registrasi.vue'
-import Posm from '../pages/Posm.vue'
-import TransactionProduct from '../pages/TransactionProduct.vue'
-
+import Home from "../pages/Home.vue";
+import Catalog from "../pages/Catalog.vue";
+import Allproduct from "../pages/Allproduct.vue";
+import Product from "../pages/Product.vue";
+import Categories from "../pages/Categories.vue";
+import Ewallet from "../pages/Ewallet.vue";
+import PoinCash from "../pages/PoinCash.vue";
+import Rebate from "../pages/Rebate.vue";
+import Pulsa from "../pages/Pulsa.vue";
+import Quarter from "../pages/Quarter.vue";
+import Login from "../pages/Login.vue";
+import ProductDetails from "../pages/ProductDetails.vue";
+import PointCashDetails from "../pages/PointCashDetails.vue";
+import RebateDetails from "../pages/RebateDetails.vue";
+import PulsaDetails from "../pages/PulsaDetails.vue";
+import EwalletDetails from "../pages/EwalletDetails.vue";
+import TransactionDetails from "../pages/TransactionDetails.vue";
+import SuccessPage from "../pages/SuccessPage.vue";
+import Registrasi from "../pages/Registrasi.vue";
+import Posm from "../pages/Posm.vue";
+import TransactionProduct from "../pages/TransactionProduct.vue";
+import TransactionRebate from "../pages/TransactionRebate.vue";
+import TransactionEwallet from "../pages/TransactionEwallet.vue";
+import TransactionPulsa from "../pages/TransactionPulsa.vue";
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/Home/:outlet_id",
-      props:true,
+    props: true,
     name: "Home",
     component: Home,
-      beforeEnter: (to, from, next) => {
-          if (localStorage.getItem("token") == null) {
-              next("/");
-          } else {
-              next();
-          }
-      },
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem("token") == null) {
+        next("/");
+      } else {
+        next();
+      }
+    },
   },
   {
     path: "/about",
@@ -45,133 +47,151 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/Catalog',
+    path: "/Catalog",
     component: Catalog,
-    name: 'catalog'
-},
-{
-    path: '/allproduct/:id',
+    name: "catalog",
+  },
+  {
+    path: "/allproduct/:id",
     component: Allproduct,
-    name: 'allproduct',
-    props: true
-},
-{
-    path: '/Categories',
+    name: "allproduct",
+    props: true,
+  },
+  {
+    path: "/Categories",
     component: Categories,
-    name: 'categories',
-    props: true
-},
-{
-    path: '/product',
+    name: "categories",
+    props: true,
+  },
+  {
+    path: "/product",
     component: Product,
-    name: 'product',
-    props: true
-},
-{
-    path: '/ewallet',
+    name: "product",
+    props: true,
+  },
+  {
+    path: "/ewallet",
     component: Ewallet,
-    name: 'ewallet',
-    props: true
-},
-{
-    path: '/PoinCash',
+    name: "ewallet",
+    props: true,
+  },
+  {
+    path: "/PoinCash",
     component: PoinCash,
-    name: 'poincash',
-    props: true
-},
-{
-    path: '/Rebate',
+    name: "poincash",
+    props: true,
+  },
+  {
+    path: "/Rebate",
     component: Rebate,
-    name: 'rebate',
-    props: true
-},
-{
-    path: '/Pulsa',
+    name: "rebate",
+    props: true,
+  },
+  {
+    path: "/Pulsa",
     component: Pulsa,
-    name: 'pulsa',
-    props: true
-},
-{
-    path: '/Quarter',
+    name: "pulsa",
+    props: true,
+  },
+  {
+    path: "/Quarter",
     component: Quarter,
-    name: 'quarter',
-    props: true
-},
-{
-    path: '/',
+    name: "quarter",
+    props: true,
+  },
+  {
+    path: "/",
     component: Login,
-    name: 'login',
-    props: true
-},
-{
-    path: '/ProductDetail',
+    name: "login",
+    props: true,
+  },
+  {
+    path: "/ProductDetail",
     component: ProductDetails,
-    name: 'productdetail',
-    props: true
-},
-{
-    path: '/PointCashDetails',
+    name: "productdetail",
+    props: true,
+  },
+  {
+    path: "/PointCashDetails",
     component: PointCashDetails,
-    name: 'pointcashdetails',
-    props: true
-},
-{
-    path: '/RebateDetails',
+    name: "pointcashdetails",
+    props: true,
+  },
+  {
+    path: "/RebateDetails",
     component: RebateDetails,
-    name: 'RebateDetails',
-    props: true
-},
-{
-    path: '/PulsaDetails',
+    name: "RebateDetails",
+    props: true,
+  },
+  {
+    path: "/PulsaDetails",
     component: PulsaDetails,
-    name: 'PulsaDetails',
-    props: true
-},
-{
-    path: '/EwalletDetails',
+    name: "PulsaDetails",
+    props: true,
+  },
+  {
+    path: "/EwalletDetails",
     component: EwalletDetails,
-    name: 'EwalletDetails',
-    props: true
-},
-{
-    path: '/TransactionDetails',
+    name: "EwalletDetails",
+    props: true,
+  },
+  {
+    path: "/TransactionDetails",
     component: TransactionDetails,
-    name: 'TransactionDetails',
-    props: true
-},
-{
-    path: '/SuccessPage',
+    name: "TransactionDetails",
+    props: true,
+  },
+  {
+    path: "/SuccessPage",
     component: SuccessPage,
-    name: 'SuccessPage',
-    props: true
-},
-{
-    path: '/Registrasi',
+    name: "SuccessPage",
+    props: true,
+  },
+  {
+    path: "/Registrasi",
     component: Registrasi,
-    name: 'Registrasi',
-    props: true
-},
-{
-    path: '/Posm',
+    name: "Registrasi",
+    props: true,
+  },
+  {
+    path: "/Posm",
     component: Posm,
-    name: 'Posm',
-    props: true
-},
-    {
-        path: '/TransactionProduct',
-        component: TransactionProduct,
-        name: 'TransactionProduct',
-        props: true
-    }
+    name: "Posm",
+    props: true,
+  },
+  {
+    path: "/TransactionProduct",
+    component: TransactionProduct,
+    name: "TransactionProduct",
+    props: true,
+  },
+  {
+    path: "/TransactionRebate",
+    component: TransactionRebate,
+    name: "TransactionRebate",
+    props: true,
+  },
+  {
+    path: "/TransactionEwallet",
+    component: TransactionEwallet,
+    name: "TransactionEwallet",
+    props: true,
+  },
+  {
+    path: "/TransactionPulsa",
+    component: TransactionPulsa,
+    name: "TransactionPulsa",
+    props: true,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
