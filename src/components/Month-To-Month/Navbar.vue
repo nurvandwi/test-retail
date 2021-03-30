dbd8167921c8de856d1793a589cd562043691883
nurvandwi <nurvandwi20@gmail.com>
2021-03-30 17:47:39 +0700

    add new component

---
+++ src/components/Month-To-Month/Navbar.vue
@@ -0,0 +1,234 @@
+<template>
+  <div>
+    <div class="mb-custom">
+      <section class="bg-section custom-position row justify-content-center">
+        <div class="col-12 col-md-12 my-3">
+          <h3
+            class="font14 text-top text-white font-weight-bolder text-center mb-0"
+          >
+            {{ kode_outlet }}
+          </h3>
+
+          <p class="font22 m-0 text-white text-center">{{ title }}</p>
+        </div>
+      </section>
+      <section>
+        <div class="box-position text-center mb-3">
+          <div class="d-flex justify-content-center">
+            <div
+              class="row col-md-10 col-10 table-bordered bg-orage border-radius"
+            >
+              <div class="col py-1 col-md-6 border-right">
+                <div>
+                  <h2 class="font11 m-0 text-white font-weight-bold pt-1">
+                    SISA POIN
+                  </h2>
+                </div>
+              </div>
+              <div class="col-6 py-1 col-md-6 m-0 p-0">
+                <div>
+                  <p class="font-weight-bold font16 text-warning m-0">
+                    10000000
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row p-0 d-flex justify-content-between">
+            <div class="content my-2 mx-auto">
+              <table class="table mb-1 mx-0">
+                <thead>
+                  <tr class="d-flex justify-content-center">
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">January</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">February</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">Maret</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">April</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">Mei</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">Juni</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">July</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">Agustus</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">September</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">Oktober</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">November</h3>
+                      </button>
+                    </th>
+                    <th scope="col">
+                      <button type="button" class="btn btn-border">
+                        <h3 class="font12 px-2 mb-0">Desember</h3>
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'kode_outlet'],
+  computed: {
+    cartItemCount () {
+      return this.$store.getters.cartItemCount
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content {
+  overflow-x: scroll;
+  scrollbar-width: none;
+  width: 80%;
+}
+.content::-webkit-scrollbar {
+  display: none;
+}
+
+.btn-light:hover {
+  background-color: #ffcc00;
+}
+
+.table th {
+  border: none;
+}
+
+.table-bordered {
+  border-color: #6a813d;
+  border-radius: 40px 40px 40px 40px;
+}
+
+.mb-custom {
+  margin-bottom: 5rem;
+}
+
+.bg-section {
+  min-height: 10vh;
+  height: 15vh;
+  background-color: #41009a;
+}
+
+.bg-orage {
+  background: #ff4814;
+}
+
+.btn-border {
+  border: 1px solid #d3d3d3;
+  border-radius: 10px;
+  background: #fff;
+}
+
+.box-position {
+  top: 11vh;
+  left: 0;
+  right: 0;
+  z-index: 1030;
+  margin-top: -4vh;
+}
+
+.border-radius {
+  border-radius: 20px, 20px, 20px, 20px;
+}
+
+/* fonts */
+
+.font11 {
+  font-size: 11px;
+}
+
+.font14 {
+  font-size: 14px;
+}
+
+.font12 {
+  font-size: 12px;
+}
+
+.font22 {
+  font-size: 22px;
+}
+
+@media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
+  .mb-custom {
+    margin-bottom: 13rem;
+  }
+
+  .custom-position {
+    min-height: 16vh;
+    background-color: #41009a;
+    position: fixed !important;
+    top: 0;
+    left: 0;
+    right: 0;
+    max-width: auto;
+    z-index: 1030 !important;
+  }
+
+  .margin-top-custom {
+    margin-top: -160px;
+  }
+
+  .box-position {
+    position: fixed;
+    top: 17vh;
+    left: 0;
+    right: 0;
+    max-width: auto;
+    z-index: 1030;
+  }
+
+  .font20 {
+    font-size: 20px;
+  }
+  .font16 {
+    font-size: 16px;
+  }
+}
+</style>
---
+++ src/components/Month-To-Month/Table.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="container">
+    <div
+      class="row text-center rows-col-3 py-3 margin-custom"
+      v-if="contentFor === 'table-penjualan'"
+    >
+      <div class="col bg-blue text-white py-2 font-custom">
+        {{ title }}
+      </div>
+      <div class="table-bordered col-12 py-2">
+        <div class="row text-center">
+          <div class="col font10" style="border-right: 1px solid #d3d3d3">
+            <h2 class="font10 text-gray font-custom font-weight-bolder">
+              TARGET PENJUALAN
+            </h2>
+          </div>
+          <div class="col">
+            <h2 class="font10 text-gray font-custom font-weight-bolder">
+              AKTUAL PENJUALAN
+            </h2>
+          </div>
+        </div>
+        <div class="row text-center mb-2 border-bottom">
+          <div class="col font10" style="border-right: 1px solid #d3d3d3">
+            <h2 class="font14 text-black font-weight-bolder">Rp 0</h2>
+          </div>
+          <div class="col">
+            <h2 class="font14 text-black font-weight-bolder">Rp 0</h2>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col font10" style="border-right: 1px solid #d3d3d3">
+            <h2 class="font10 text-gray font-custom font-weight-bolder">
+              SELISIH PENJUALAN
+            </h2>
+          </div>
+          <div class="col">
+            <h2 class="font10 text-gray font-custom font-weight-bolder">
+              RATIO PENJUALAN
+            </h2>
+          </div>
+        </div>
+        <div class="row text-center mb-2">
+          <div class="col font10" style="border-right: 1px solid #d3d3d3">
+            <h2 class="font14 text-black font-weight-bolder">Rp 0</h2>
+          </div>
+          <div class="col">
+            <h2 class="font14 text-black font-weight-bolder">0 %</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!------------------------------------------------------------------------>
+    <div
+      class="row text-center rows-col-3 py-3 margin-custom"
+      v-if="contentFor === 'AO'"
+    >
+      <div class="col bg-blue text-white py-2 font-custom">
+        {{ title }}
+      </div>
+      <div class="table-bordered col-12 py-2">
+        <div class="row text-center">
+          <div class="col font10" style="border-right: 1px solid #d3d3d3">
+            <h2 class="font10 text-gray font-custom font-weight-bolder">AO</h2>
+          </div>
+          <div class="col">
+            <h2 class="font10 text-gray font-custom font-weight-bolder">RO</h2>
+          </div>
+        </div>
+        <div class="row text-center mb-2 border-bottom">
+          <div class="col font10" style="border-right: 1px solid #d3d3d3">
+            <h2 class="font14 text-black font-weight-bolder">0</h2>
+          </div>
+          <div class="col">
+            <h2 class="font14 text-black font-weight-bolder">0</h2>
+          </div>
+        </div>
+        <div class="row text-center mt-2">
+          <div class="col font10 align-self-center">
+            <h2 class="font10 text-gray font-weight-bolder font-custom">
+              AO/RO
+            </h2>
+          </div>
+          <div class="col">
+            <h2 class="font14 text-black font-weight-bolder">0 %</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!------------------------------------------------------------------------>
+    <div class="row text-center mb-quarter" v-if="contentFor === 'Total'">
+      <div class="col-12 table-top col-md-12 bg-blue d-flex px-1">
+        <div
+          class="font12 p-0 text-white text-center border-right col-sm-6 col-6 py-2"
+        >
+          TYPE KPI
+        </div>
+        <div
+          class="p-0 font12 text-white border-right text-center col-sm-6 col-6 py-2"
+        >
+          POIN
+        </div>
+      </div>
+      <div class="col-12 table-bordereds rounded-bottom">
+        <div class="row mt-2">
+          <div class="col-6 col-md-6 px-0 p-0">
+            <h2
+              class="font12 col-12 px-1 text-gray text-left font-weight-bolder"
+            >
+              KPI SELL IN
+            </h2>
+          </div>
+          <div class="col-6 col-md-6 text-center">
+            <h2 class="font12 font-weight-bolder">0</h2>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col-6 col-md-06 px-0">
+            <h2
+              class="font12 px-1 col-12 font-custom text-left text-gray font-weight-bolder"
+            >
+              KPI SELL OUT
+            </h2>
+          </div>
+          <div class="col-6 col-md-6 text-center">
+            <h2 class="font12 position-relative font-weight-bolder">0</h2>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col-6 col-md-06 px-0">
+            <h2
+              class="font12 px-1 col-12 font-custom text-left text-gray font-weight-bolder"
+            >
+              HERO CATEGORY
+            </h2>
+          </div>
+          <div class="col-6 col-md-6 text-center">
+            <h2 class="font12 position-relative font-weight-bolder">0</h2>
+          </div>
+        </div>
+        <div class="row mt-2 border-bottom">
+          <div class="col-6 col-md-06 px-0">
+            <h2
+              class="font12 px-1 col-12 font-custom text-left text-gray font-weight-bolder"
+            >
+              AO/RO
+            </h2>
+          </div>
+          <div class="col-6 col-md-6 text-center">
+            <h2 class="font12 text-black font-weight-bolder">0</h2>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col-6 col-md-06 px-0">
+            <h2
+              class="font12 px-1 col-12 font-custom text-left text-gray font-weight-bolder"
+            >
+              AO/JOSS
+            </h2>
+          </div>
+          <div class="col-6 col-md-6 text-center">
+            <h2 class="font12 position-relative font-weight-bolder text-gray">
+              0
+            </h2>
+          </div>
+        </div>
+        <div class="row mt-2 border-bottom">
+          <div class="col-6 col-md-06 px-0">
+            <h2
+              class="font12 px-1 col-12 font-custom text-left text-gray font-weight-bolder"
+            >
+              TOTAL POIN
+            </h2>
+          </div>
+          <div class="col-6 col-md-6 text-center">
+            <h2 class="font12 position-relative font-weight-bolder">0</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['contentFor', 'title']
+}
+</script>
+
+<style scoped>
+.text-ungu {
+  color: #41009a;
+}
+
+.bg-blue {
+  background-color: #6a813d;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+}
+
+.margin-custom {
+  margin-top: -13vh;
+}
+
+.mt-custom {
+  margin-top: -2rem;
+}
+
+.table-bordereds {
+  border: 1px solid #6a813d;
+  border-bottom-left-radius: 12px !important;
+  border-bottom-right-radius: 12px !important;
+}
+
+.text-blue {
+  color: #7fceeb;
+}
+
+.text-gray {
+  color: #6c757d;
+}
+
+.text-yellow {
+  color: #ffcc00;
+}
+
+.font10 {
+  font-size: 10px;
+}
+
+.font12 {
+  font-size: 12px;
+}
+
+.font13 {
+  font-size: 13px;
+}
+
+.font14 {
+  font-size: 14px;
+}
+
+.mb-quarter {
+  margin-bottom: 7rem;
+}
+.rounded {
+  border-top-left-radius: 18px;
+  border-top-right-radius: 18px;
+}
+
+@media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
+  .margin-custom {
+    margin-top: -2vh;
+  }
+}
+</style>
