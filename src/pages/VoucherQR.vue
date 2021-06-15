68a91ab5998b117e699d506e4f1246f3c54ec4c4
nurvandwi <nurvandwi20@gmail.com>
2021-06-15 16:07:56 +0700

    update design card

--- src/pages/VoucherQR.vue
+++ src/pages/VoucherQR.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="containe">
     <Header :title="'Voucher Cashback'" />
     <div class="mt-custom mb-3">
       <div class="row">
@@ -21,71 +21,143 @@
         </div>
       </div>
     </div>
-    <div class="card" style="max-width: 330px">
-      <div
-        class="row no-gutters"
-        style="
-          background: whitesmoke;
-          border-top-left-radius: 15px;
-          border-bottom-left-radius: 15px;
-        "
-      >
-        <div class="col-4">
-          <div class="card-body p-0">
+    <div
+      class="card"
+      style="
+        background: linear-gradient(to right, #a2a2a2, #5f5f5f);
+        max-width: 330px;
+      "
+    >
+      <div class="container">
+        <div class="row text-white">
+          <div class="col-8 p-3">
+            <h6 class="m-0">Voucher Cashback</h6>
+            <p class="font-weight-bold m-0" style="font-size: 33px">
+              Rp. 100.000
+            </p>
+          </div>
+          <div class="col-4 pt-3 pl-4">
+            <img height="30" src="../assets/pz.png" />
+          </div>
+        </div>
+      </div>
+      <div class="d-flex">
+        <div class="col-8">
+          <div
+            class="font-weight-thin float-right text-white"
+            style="font-size: 12px"
+          >
+            ALS10A07 - ADIJAYA MAKMUR SANJAYA PERKOSA
+          </div>
+          <div
+            class="font-weight-bold float-left text-white py-2"
+            style="font-size: 12px"
+          >
+            Periode: <span class="font-weight-bold"> Desember 2021</span>
+          </div>
+        </div>
+        <div class="col-4 mb-4 pl-4">
+          <div class="float-left" style="background: #9c9c9c; padding: 5px">
             <img
-              height="50"
-              src="../assets/COG-LOGO-Joss_Retail_salesman.png"
+              width="55"
+              src="https://img.icons8.com/metro/26/000000/qr-code.png"
             />
-
-            <p
-              class="card-text mx-3 mt-3 text-muted"
-              style="border: 1px solid; border-radius: 20px; padding: 7px"
-            >
-              ALS10A07 - ADIJAYA MAKMUR
-            </p>
-            <p
-              style="white-space: pre; font-size: 13px"
-              class="card-text text-center mx-5 mb-0"
-            >
-              VOUCHER CASHBACK
-            </p>
-            <p class="card-text mx-3">
-              <small
-                class="font-weight-bold"
-                style="white-space: pre; font-size: 35px"
-                >Rp. 100.000</small
-              >
-            </p>
           </div>
         </div>
-        <div class="col-4">
-          <div class="card-body text-center p-0">
-            <img class="py-2" height="50" src="../assets/pz.png" />
+      </div>
+    </div>
+    <div
+      class="card my-2"
+      style="
+        background: linear-gradient(to right, #a2a2a2, #5f5f5f);
+        max-width: 330px;
+        max-height: 34vh;
+      "
+    >
+      <div class="container">
+        <div class="row text-white">
+          <div class="col-8 p-3">
+            <h6 class="m-0">Voucher Cashback</h6>
+            <p class="font-weight-bold m-0" style="font-size: 33px">
+              Rp. 100.000
+            </p>
+          </div>
+          <div class="col-4 pt-3 pl-4">
+            <img height="30" src="../assets/pz.png" />
           </div>
         </div>
+      </div>
+      <div class="d-flex">
+        <div class="col-7">
+          <div
+            class="font-weight-thin float-right text-white"
+            style="font-size: 12px"
+          >
+            ALS10A07 - ADIJAYA MAKMUR SANJAYA PERKOSA
+          </div>
+          <div
+            class="font-weight-bold float-left text-white py-2"
+            style="font-size: 12px"
+          >
+            Periode: <span class="font-weight-bold"> Desember 2021</span>
+          </div>
+        </div>
+        <div class="col-5 mb-3 ml-0">
+          <div
+            class="float-left text-center"
+            style="
+              background: white;
+              padding: 5px;
+              border-radius: 11px;
+              font-size: 12px;
+            "
+          >
+            <span style="font-size: 12px">Kode Voucher </span> ABCDE123456
+            <span> 17/08/2021</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="card my-3"
+      style="
+        background: linear-gradient(to right, #a2a2a2, #5f5f5f);
+        max-width: 330px;
+      "
+    >
+      <div class="container">
+        <div class="row text-white">
+          <div class="col-8 p-3">
+            <h6 class="m-0">Periode: <span> Desember 2021</span></h6>
+          </div>
+          <div class="col-4 pt-3 pl-4">
+            <img height="30" src="../assets/pz.png" />
+          </div>
+        </div>
+      </div>
+      <div class="col-12 pb-0 pt-1">
         <div
-          class="col-4 border-left"
-          style="
-            background: rgb(156 156 156);
-            border-top-right-radius: 10px;
-            border-bottom-right-radius: 10px;
-          "
+          class="font-weight-bold float-right text-white"
+          style="font-size: 11px; padding: 15px"
         >
-          <div class="card-body p-0 text-white text-center my-5">
-            <p class="card-text m-0 font-weight-bold" style="font-size: 13px">
-              Scan QRcode
-            </p>
-            <p class="card-text" style="font-size: 10px">
-              Untuk Penukaran Poin
-            </p>
-            <div class="card-text">
-              <img
-                class="p-2"
-                style="background: #fff"
-                height="70"
-                src="https://img.icons8.com/metro/26/000000/qr-code.png"
-              />
-            </div>
+          ALS10A07 - ADIJAYA MAKMUR SANJAYA PERKOSA
+        </div>
+      </div>
+      <div class="col-12 text-white align-self-center d-flex">
+        <div class="col-3 p-0 text-center bg-white my-3">
+          <!-- <p class="text-dark m-0" style="font-size: 12px">Scan QrCode</p> -->
+          <img
+            width="45"
+            class="py-3"
+            src="https://img.icons8.com/metro/26/000000/qr-code.png"
+          />
+        </div>
+        <div class="col-9 align-self-center p-0 d-block">
+          <div class="float-right">
+            <p class="m-0">Voucher Cashback</p>
+          </div>
+          <div class="font-weight-bold float-right" style="font-size: 35px">
+            Rp. 100.000
           </div>
         </div>
       </div>
