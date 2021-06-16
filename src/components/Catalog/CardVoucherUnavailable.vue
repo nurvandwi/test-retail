1a79ca158d67af09d9dd3170bde8804c857f682a
nurvandwi <nurvandwi20@gmail.com>
2021-06-16 16:19:03 +0700

    update tabs

---
+++ src/components/Catalog/CardVoucherUnavailable.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    class="card mt-3 shadow-lg mx-auto"
+    style="
+      background: linear-gradient(to right, #a2a2a2, #5f5f5f);
+      max-width: 330px;
+      max-height: 34vh;
+    "
+  >
+    <div class="container">
+      <div class="row text-white">
+        <div class="col-8 p-3">
+          <h6 class="m-0">Voucher Cashback</h6>
+          <p class="font-weight-bold m-0" style="font-size: 33px">
+            Rp. 100.000
+          </p>
+        </div>
+        <div class="col-4 pt-3 pl-4">
+          <img height="30" src="../../assets/pz.png" />
+        </div>
+      </div>
+    </div>
+    <div class="d-flex">
+      <div class="col-7">
+        <div
+          class="font-weight-thin float-right text-white"
+          style="font-size: 12px"
+        >
+          ALS10A07 - ADIJAYA MAKMUR SANJAYA PERKOSA
+        </div>
+        <div
+          class="font-weight-bold float-left text-white py-2"
+          style="font-size: 12px"
+        >
+          Periode: <span class="font-weight-bold"> Desember 2021</span>
+        </div>
+      </div>
+      <div class="col-5 mb-3 ml-0">
+        <div
+          class="float-left text-center"
+          style="
+            background: white;
+            padding: 5px;
+            border-radius: 11px;
+            font-size: 12px;
+          "
+        >
+          <span style="font-size: 12px">Kode Voucher </span> ABCDE123456
+          <span> 17/08/2021</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
