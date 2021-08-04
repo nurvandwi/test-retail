7d51d115298291da13d3128ba14b0c9b7c6b9400
nurvandwi <nurvandwi20@gmail.com>
2021-08-04 11:22:35 +0700

    update get data voucher

--- src/components/Catalog/CardVoucherAvailable.vue
+++ src/components/Catalog/CardVoucherAvailable.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container mt-3">
+  <div class="container mt-3 mb-5">
     <div
-      class="card shadow-lg mx-auto max-width bg_custom"
+      class="card shadow-lg mx-auto max-width bg_custom my-3"
       :style="backgroundColor"
+      v-for="voucher in dataVouchers"
+      :key="voucher"
     >
       <div class="container">
         <div class="row text-white">
           <div class="col-8 p-3">
             <h6 class="m-0">Voucher Cashback</h6>
-            <p class="font-weight-bold m-0" style="font-size: 33px">
-              Rp. 100.000
+            <p class="font-weight-bold m-0" style="font-size: 18px">
+              {{ voucher.nama_produk }}
             </p>
           </div>
           <div class="col-4 pt-3 pl-4">
@@ -19,17 +21,15 @@
       </div>
       <div class="d-flex">
         <div class="col-8">
-          <div
-            class="font-weight-thin float-right text-white"
-            style="font-size: 13px"
-          >
-            ALS10A07 - ADIJAYA MAKMUR SANJAYA PERKOSA
+          <div class="font-weight-thin text-white" style="font-size: 13px">
+            {{ voucher.outlet_id }} - {{ voucher.outlet_name }}
           </div>
           <div
             class="font-weight-bold float-left text-white py-2"
             style="font-size: 12px"
           >
-            Periode: <span class="font-weight-bold"> Desember 2021</span>
+            Periode:
+            <span class="font-weight-bold"> {{ voucher.periode }}</span>
           </div>
         </div>
         <div class="col-4 mb-4 pl-4">
@@ -46,8 +46,39 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   props: ["color"],
+  data() {
+    return {
+      dataVouchers: [],
+    };
+  },
+  methods: {
+    getVoucher() {
+      axios
+        .get(
+          "https://pzc.inosis.id/api_pzc_retail_2021/api.php/list-qr-tersedia_rtl",
+          {
+            headers: {
+              token: localStorage.token,
+            },
+            params: {
+              page: 0,
+            },
+          }
+        )
+        .then((res) => {
+          this.dataVouchers = res.data.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    this.getVoucher();
+  },
   computed: {
     backgroundColor() {
       return `background-image: ${this.color}`;
